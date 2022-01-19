e601179ac22a5426b1b0884c797554453b7bd1ff
JuniorAlvess <junioralves.jav@gmail.com>
2022-01-19 18:33:42 -0300

    In progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import photo from './assets/mario.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <nav>
+          <ul>
+            <li>Work</li>
+            <li>Resume</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+
+        <section>
+          <img src={photo} alt="Junior" width={450} height={500} />
+          <article>
+            <h1>Hi, I'm Junior and I'm a FullStack Developer.</h1>
+            <p>
+              Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris
+              se pirulitá.Interagi no mé, cursus quis, vehicula ac nisi.Delegadis gente finis,
+              bibendum egestas augue arcu ut est.Paisis, filhis, espiritis santis.
+            </p>
+            <button>Contact me</button><button>Check my work</button>
+          </article>
+        </section>
       </header>
     </div>
   );
