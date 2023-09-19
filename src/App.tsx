ef4b78346edcdcbb7dbb122bbf975d84f2aee747
Junior Alves <junioralves.jav@gmail.com>
2023-09-19 13:56:22 -0300

    chore: updated resume

--- src/App.tsx
+++ src/App.tsx
@@ -237,7 +237,7 @@ function App() {
             </ol>
           </section>
 
-          <a href="/assets/Ednaldo_Alves_Resume.pdf" download>Download CV (PDF)</a>
+          <a href="/assets/junior_alves_resume.pdf" download>Download CV (PDF)</a>
         </section>
         <section>
           <h2 id="contact">Contact</h2>
